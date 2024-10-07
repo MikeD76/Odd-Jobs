2f7dbd41e8a47fc67549274bb48b02c2bbd945d6
MikeD76 <madais@csuchico.edu>
2024-10-07 12:45:10 -0700

    Took everything out of oddjobs folder and put it in root directory

---
+++ app/JobForum.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function JobForum() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Jobs Posted:</Text>
+      {/* Add more components here to show job postings */}
+    </View>
+  );
+}
